448e86c7569f802847d7acb00e291a9821dd74a3
DharunPrasad <61703845+DharunPrasad@users.noreply.github.com>
2023-02-03 01:46:04 +0530

    Updated as PWA

--- src/components/Age.js
+++ src/components/Age.js
@@ -1,8 +1,6 @@
 import { useEffect, useRef } from "react";
 
 const Age = ({ data, setAge, setDisplayIncome, age }) => {
-    const ageInput = useRef()
-    const ageForm = useRef();
 
     const handleChange = (e) => {
         setAge(e.target.value)
@@ -10,19 +8,16 @@ const Age = ({ data, setAge, setDisplayIncome, age }) => {
     }
 
 
-    useEffect(() => {
-      ageInput.current.focus()
-    },[])
- 
+
     return (
       
-    <form action="" className="bg-chatbg p-4 w-10/12 mx-auto rounded-xl my-5" ref = {ageForm}>
+    <form action="" className="bg-chatbg p-4 w-10/12 mx-auto rounded-xl my-5">
       <p className="text-lg font-semibold text-center mb-4">Enter Your Age</p>
       <div className="w-full border-t border-gray-400"></div>
       <input
         type="number"
         value={age}
-        ref = {ageInput}
+  
         onChange = {handleChange}
         className="block mx-auto rounded-md m-3 text-center w-3/12 h-10 outline-none p-2 border"
       />
--- src/pages/Scheme.js
+++ src/pages/Scheme.js
@@ -6,10 +6,10 @@ const Scheme = () => {
         <div className="min-h-screen w-full">
             <Navbar/>
         <div className="bg-chatbg p-3 rounded-lg w-11/12 mx-auto mt-3" >
-        <Link to = "" className="">
+        <a href = "https://www.myscheme.gov.in/find-scheme/v3" rel="noreferrer" target="_blank" className="">
           <p className="text-primary font-semibold">Scheme for you</p>
           <p className="">This is your scheme</p>
-        </Link>
+        </a>
       </div>
       </div>
       );
