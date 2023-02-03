974c0a829ad71b9db60769bcf924d77aba71cf27
DharunPrasad <61703845+DharunPrasad@users.noreply.github.com>
2023-02-03 08:36:03 +0530

    Changed redirect link

--- src/pages/Scheme.js
+++ src/pages/Scheme.js
@@ -6,7 +6,7 @@ const Scheme = () => {
         <div className="min-h-screen w-full">
             <Navbar/>
         <div className="bg-chatbg p-3 rounded-lg w-11/12 mx-auto mt-3" >
-        <a href = "https://www.myscheme.gov.in/find-scheme/v3" rel="noreferrer" target="_blank" className="">
+        <a href = "https://www.myscheme.gov.in/" rel="noreferrer" target="_blank" className="">
           <p className="text-primary font-semibold">Scheme for you</p>
           <p className="">This is your scheme</p>
         </a>
