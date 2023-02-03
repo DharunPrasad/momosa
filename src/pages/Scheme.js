55c4f96d2beb2e3a98f2319e90e92690a20fd126
DharunPrasad <61703845+DharunPrasad@users.noreply.github.com>
2023-02-03 10:38:09 +0530

    Changed the redirect lnk

--- src/pages/Scheme.js
+++ src/pages/Scheme.js
@@ -6,9 +6,9 @@ const Scheme = () => {
         <div className="min-h-screen w-full">
             <Navbar/>
         <div className="bg-chatbg p-3 rounded-lg w-11/12 mx-auto mt-3" >
-        <a href = "https://www.myscheme.gov.in/" rel="noreferrer" target="_blank" className="">
-          <p className="text-primary font-semibold">Scheme for you</p>
-          <p className="">This is your scheme</p>
+        <a className="">
+          <p className="text-primary font-semibold">Girl Incentive scheme</p>
+          <p className="">Adi Dravidar and Tribal Welfare Department</p>
         </a>
       </div>
       </div>
